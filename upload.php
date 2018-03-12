87e7568d4b89be009164b64e99e4cd63bc4c567e
Ankur Dua <ankurdua15@gmail.com>
2018-03-12 23:39:17 +0530

    page_2

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php
+    
+    
+    $file=$_FILES["file"];
+    $target_dir="uploads/a.jpg";
+    if ($file['error'] > 0) 
+    {
+        die('An error occured while upload error code=' . $file['error']);
+    }
+    $status=  move_uploaded_file($file["tmp_name"], $target_dir);
+    if(!$status)
+    {
+        die('An error occured during move<br>'.$target_dir.$file['error']);
+    }
+    else
+    {
+        die('File upload successful');
+    }
+?>
