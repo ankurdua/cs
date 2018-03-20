1202d4664e5508625185895a542d01c2ee6b19d1
Ankur Dua <ankurdua15@gmail.com>
2018-03-21 01:31:12 +0530

    email testing 2.1

--- test2/mail.php
+++ test2/mail.php
@@ -1,15 +1,17 @@
 
 <?php
-    require_once 'vendor/swiftmailer/swiftmailer/lib/swift_required.php';
+    require 'vendor/autoload.php';
     try
     {
-        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465,'ssl')->setUsername($_ENV['gmailUsername'])->setPassword($_ENV['gmalPassword']);
+        $transport = new Swift_SmtpTransport('smtp.gmail.com', 465,'ssl');
+        $transport->setUsername($_ENV['gmailUsername']);
+        $transport->setPassword($_ENV['gmailPassword']);
 
-        $mailer = Swift_Mailer::newInstance($transport);
-        $message = Swift_Message::newInstance('test email')
-        ->setFrom(array('hostelallotmentism@gmail.com' => 'Our Code World'))
-        ->setTo(array("ankurdua15@gmail.com" => "mail@mail.com"))
-        ->setBody("<h1>Welcome</h1>", 'text/html');
+        $mailer = new Swift_Mailer($transport);
+        $message = new Swift_Message('test email');
+        $message->setFrom(array('hostelallotmentism@gmail.com' => 'Our Code World'));
+        $message->setTo(array("ankurdua15@gmail.com" => "mail@mail.com"));
+        $message->setBody("<h1>Welcome</h1>", 'text/html');
         $result = $mailer->send($message);
         if($result)
         {
