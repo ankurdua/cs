4c2cc74e5455fea461f7f1389fd3fa8af7112a09
Ashutosh Shekhar <ashutoshshekhar553@gmail.com>
2018-03-22 22:25:37 +0530

    Error handling added

--- upload.php
+++ upload.php
@@ -13,9 +13,28 @@
         if ($file['error'] > 0) 
         {
             echo '<script type=text/javascript>alert("An error occured. Please try again");</script>';
-            exit();
-            
+            exit();  
         }
+		
+		//
+		
+		//checking image file type.
+		$directory=dirname(__FILE__)."/uploads/";
+		$target_file=$directory. basename($file["name"]);
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		if($imageFileType != "jpg" && $imageFileType != "jpeg") 
+		{
+             echo '<script type=text/javascript>alert("You are allowed to upload only jpg and jpeg images");</script>';
+             exit();
+        }
+		//checking image size.
+		if ($file["size"] > 1000000) 
+		{
+            echo '<script type=text/javascript>alert("Image size should be less then 1MB");</script>';
+            exit();
+        }
+	//
+	
         $status=  move_uploaded_file($file["tmp_name"], $target_dir);
         if(!$status)
         {
@@ -33,6 +52,7 @@
             }
             mysqli_select_db($con, $db_name);
             $sql="INSERT INTO file VALUES('".$_SESSION['user']."');";
+
             if(!mysqli_query($con, $sql))
             {
                 echo '<script type=text/javascript>alert("An error occured. Please try again");</script>';
